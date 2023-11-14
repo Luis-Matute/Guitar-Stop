2cfb83470d33840c7748cba4b9580f04ae4e37c1
Luis Matute <luis.matute@perficient.com>
2023-11-14 16:45:03 -0600

    Added routing logic and a tool bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './shopping/product-list/product-list.component';
+import { ProductDetailsComponent } from './shopping/product-details/product-details.component';
+import { CartComponent } from './cart/cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout/checkout.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'shop', component: ProductListComponent},
+  {path: 'details/:id', component: ProductDetailsComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'checkout', component: CheckoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
