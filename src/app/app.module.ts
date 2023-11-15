fd066e9128986deec993ac9d1aebd22456f3a6d6
Luis Matute <luis.matute@perficient.com>
2023-11-15 13:34:46 -0600

    Modified service to fetch products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 
+import { ShoppingModule } from './shopping/shopping.module';
+import { CheckoutModule } from './checkout/checkout.module';
+import { CartModule } from './cart/cart.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +26,13 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    HttpClientModule,
+    MatSlideToggleModule,
+    CommonModule,
+    ShoppingModule,
+    CheckoutModule,
+    CartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
