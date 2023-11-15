1ec097b8bfe7ad947bfc39a78d2c173f8fd3b05d
Luis-Matute <97914974+Luis-Matute@users.noreply.github.com>
2023-11-15 15:54:18 -0600

    Added product details page with a 404 not found page (#7)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,11 +16,13 @@ import { HomeComponent } from './home/home.component';
 import { ShoppingModule } from './shopping/shopping.module';
 import { CheckoutModule } from './checkout/checkout.module';
 import { CartModule } from './cart/cart.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shopping/product-details/product-details.component.ts
+++ src/app/shopping/product-details/product-details.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from '../product.service';
+import { Guitar } from 'src/app/models/guitar';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-product-details',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent {
+
+  product: Guitar = {} as Guitar;
+
+  constructor(private productService: ProductService, private router: Router) { }
+
+  @Input()
+  set id(id: string) {
+    this.productService.getProduct(id).subscribe({
+      next: (result) => {
+        console.log("Response:");
+        console.log(result);
+        this.product = result;
+      },
+      error: (e: HttpErrorResponse) => {
+        console.log(e.error.message);
+        console.log(e.message);
+        console.log(e.status);
+        this.router.navigate(['**']);
+      },
+      complete: () => console.log('Complete')
+    });
+  }
 
 }
