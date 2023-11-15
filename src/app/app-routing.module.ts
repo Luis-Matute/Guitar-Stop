d3512bf814dbad15913aa04f863435dcf6d7a79c
Luis Matute <luis.matute@perficient.com>
2023-11-15 15:52:55 -0600

    Added product details page with a 404 not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, provideRouter, withComponentInputBinding } from '@angular/router';
 import { ProductListComponent } from './shopping/product-list/product-list.component';
 import { ProductDetailsComponent } from './shopping/product-details/product-details.component';
 import { CartComponent } from './cart/cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout/checkout.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
@@ -12,11 +13,13 @@ const routes: Routes = [
   {path: 'shop', component: ProductListComponent},
   {path: 'details/:id', component: ProductDetailsComponent},
   {path: 'cart', component: CartComponent},
-  {path: 'checkout', component: CheckoutComponent}
+  {path: 'checkout', component: CheckoutComponent},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [provideRouter(routes, withComponentInputBinding())]
 })
 export class AppRoutingModule { }
