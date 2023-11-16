751ceeaa9b0a1f85724c592ffe7e6513e04d1864
Luis-Matute <97914974+Luis-Matute@users.noreply.github.com>
2023-11-16 10:47:54 -0600

    Added cart view (#9)

--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Cart } from '../models/cart';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getCart(userId: string): Observable<Cart> {
+    return this.http.get<Cart>(`${this.apiUrl}/cart/${userId}`);
+  }
+
+  clearCart(cartId: string): Observable<Cart> {
+    return this.http.delete<Cart>(`${this.apiUrl}/clearCart/${cartId}`);
+  }
+
+
 }
